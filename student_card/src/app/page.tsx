86ef76cdc2d0201cd5c61fac330ebc80abafb20e
Niki <szilviniki9@gmail.com>
2025-02-02 10:59:19 +0100

    .

--- student_card/src/app/page.tsx
+++ student_card/src/app/page.tsx
@@ -1,15 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import OneCard from "@/components/OneCard";
 import {Col, Row} from "react-bootstrap";
 
 export default function Home() {
-  return (
-<Row>
-  <Col>
-    <OneCard front="valami" back="csdcwe" />
-  </Col>
-</Row>
-
-  );
+    return (
+        <Row>
+            <Col>
+                <OneCard front="valami" back="csdcwe"/>
+            </Col>
+        </Row>
+    );
 }
