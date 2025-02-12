0fc5649261b3a3e3fc2741ed0fc0de1bd3416d39
oliver balassa <balassa.oliver@outlook.com>
2025-02-12 15:24:57 +0100

    Created UserService and UserController
    added routes:
    /users
    /users/find?id=[User's ID]

--- spring-backend/src/main/java/hu/devoli/springbackend/controllers/UserController.java
+++ spring-backend/src/main/java/hu/devoli/springbackend/controllers/UserController.java
@@ -3,12 +3,15 @@ package hu.devoli.springbackend.controllers;
 import hu.devoli.springbackend.entities.User;
 import hu.devoli.springbackend.entities.dataaccess.UserRepository;
 import hu.devoli.springbackend.records.UserResponse;
+import hu.devoli.springbackend.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.print.Pageable;
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,25 +22,25 @@ import java.util.UUID;
 public class UserController {
 
     private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<String> Hello(){
+        return ResponseEntity.ok("this is the /users route!");
     }
 
     @GetMapping("/find")
     public Optional<ResponseEntity<UserResponse>> findById(@RequestParam(value = "id")UUID id){
-        try {
-            Optional<User> user = userRepository.findById(id);
-            if(user.isPresent()){
-               UserResponse res = new UserResponse(user.get().getId(), user.get().getName(), user.get().getUsername());
-               return Optional.of(ResponseEntity.ok(res));
-            }else{
-                throw new Exception("User not found");
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
+      return userService.findById(id);
     }
+
+//    @GetMapping("/index")
+//    public ResponseEntity<Pageable> Index(@RequestParam(name = "amount", defaultValue = "20") int amount){
+//        Iterable<User> queryResults = userRepository.findAll();
+//    }
 }
---
+++ spring-backend/src/main/java/hu/devoli/springbackend/services/UserService.java
@@ -0,0 +1,44 @@
+package hu.devoli.springbackend.services;
+
+import hu.devoli.springbackend.entities.User;
+import hu.devoli.springbackend.entities.dataaccess.UserRepository;
+import hu.devoli.springbackend.records.UserResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Transient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Transient
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<ResponseEntity<UserResponse>> findById(UUID id){
+        try {
+            java.util.Optional<User> user = userRepository.findById(id);
+            if(user.isPresent()){
+                UserResponse res = new UserResponse(user.get().getId(), user.get().getName(), user.get().getUsername());
+                return java.util.Optional.of(ResponseEntity.ok(res));
+            }else{
+                throw new Exception("User not found");
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+//    public Page<UserResponse> getusersPage(){
+//
+//    }
+
+}
